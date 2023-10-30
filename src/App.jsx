6a14e260f7d66ec9bec4a796041b3ce30b1a207a
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2023-10-31 01:31:14 +0530

    App jsx updated.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
+import Footer from './components/Footer'
+import Dashboard from './pages/Dashboard'
 
 const App = () => {
   return (
     <section className="app">
       <Navbar />
-      <Home />
+      {/* <Home /> */}
+      <Dashboard/>
+      <Footer />
     </section>
   )
 }
