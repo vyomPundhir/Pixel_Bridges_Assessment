59e84b151c8346f9dd68a3149dd3ebe8a2025609
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2023-10-31 19:22:43 +0530

    App jsx file updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import Footer from './components/Footer'
 import Dashboard from './pages/Dashboard'
+import Earn from './pages/Earn'
 
 const App = () => {
   return (
     <section className="app">
       <Navbar />
       {/* <Home /> */}
-      <Dashboard/>
+      {/* <Dashboard/> */}
+      <Earn/>
       <Footer />
     </section>
   )
